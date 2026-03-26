c50a52ea388e139ce851a3c99d8db21584e289dc
Silvana Nova <silvana.an@live.cl>
2026-03-26 11:35:41 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+let nombre = "Javiera";
+let activo =true;
+let a = 20;
+let b = 5;
+let mensaje = "Aplicando Javascript";
+
+let suma = a + b;           
+let resta = a - b;         
+let multi = a * b;          
+let division = a / b;
+
+
+console.log("nombre:" + nombre);
+console.log("activo:" + activo);
+console.log("a" + a);
+console.log("b" + b);
+console.log("mensaje:" + mensaje);
+
+
+console.log("suma:", suma);
+console.log("resta:", resta);
+console.log("multi:", multi);
+console.log("división:", division);
+  
+let datos = ["Javiera", true, 20, 5, "Aplicando Javascript"];
+ alert("Bienvenidos");
+
+console.log("arreglo:", datos)
